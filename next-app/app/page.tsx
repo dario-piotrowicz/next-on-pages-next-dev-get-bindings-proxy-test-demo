4f344f721239cbd267695638980f8ef4576ea9b1
Dario Piotrowicz <dario@cloudflare.com>
2024-01-22 18:36:48 +0000

    update setupDevBindings usage

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,33 +1,44 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export default async function Home() {
-  const key = 'my-key';
+  const key = "my-key";
   const myKv = process.env.MY_KV;
-  await myKv.put(key, 'my-value');
+  await myKv.put(key, "my-value");
   const value = await myKv.get(key);
 
-  const workerResp = await process.env.MY_WORKER.fetch('http://localhost');
-  const workerRespText = await workerResp.text();
+  let workerRespText = "ERROR: no MY_WORKER found";
+  if (process.env.MY_WORKER) {
+    const workerResp = await process.env.MY_WORKER.fetch("http://localhost");
+    workerRespText = await workerResp.text();
+  }
 
-  const myDo = process.env.MY_DO;
-
-  const doId = myDo.idFromName('my-do-name');
-  const doObj = myDo.get(doId);
-  const doResp = await doObj.fetch('http://0.0.0.0');
-  const doRespText = await doResp.text();
+  let doRespText = "ERROR: no MY_DO found";
+  try {
+    const myDo = process.env.MY_DO;
+    const doId = myDo.idFromName("my-do-name");
+    const doObj = myDo.get(doId);
+    const doResp = await doObj.fetch("http://localhost");
+    doRespText = await doResp.text();
+  } catch {}
 
   return (
     <main className={styles.main}>
-       <p> MY_KV[{key}] = {JSON.stringify(value)}</p>
-       <p> MY_VAR = {JSON.stringify(process.env.MY_VAR)}</p>
-       <p> MY_JSON_VAR = {JSON.stringify(process.env.MY_JSON_VAR)}</p>
-       <p> MY_WORKER response = {JSON.stringify(workerRespText)}</p>
-       <p> MY_DO response = {JSON.stringify(doRespText)}</p>
-       <p> MY_D1 typeof exec = {typeof process.env.MY_D1.exec}</p>
-       <p> MY_R2 typeof createMultipartUpload = {typeof process.env.MY_R2.createMultipartUpload}</p>
+      <p>
+        {" "}
+        MY_KV[{key}] = {JSON.stringify(value)}
+      </p>
+      <p> MY_VAR = {JSON.stringify(process.env.MY_VAR)}</p>
+      <p> MY_JSON_VAR = {JSON.stringify(process.env.MY_JSON_VAR)}</p>
+      <p> MY_WORKER response = {JSON.stringify(workerRespText)}</p>
+      <p> MY_DO response = {JSON.stringify(doRespText)}</p>
+      <p> MY_D1 typeof exec = {typeof process.env.MY_D1.exec}</p>
+      <p>
+        {" "}
+        MY_R2 typeof createMultipartUpload ={" "}
+        {typeof process.env.MY_R2.createMultipartUpload}
+      </p>
     </main>
-  )
+  );
 }
--- next-app/next.config.js
+++ next-app/next.config.js
@@ -5,45 +5,5 @@ module.exports = nextConfig
 
 if (process.env.NODE_ENV === 'development') {
 	const { setupDevBindings } = require('@cloudflare/next-on-pages/next-dev');
-
-	setupDevBindings({
-        bindings: {
-            'MY_KV': {
-                type: 'kv',
-                id: 'MY_KV'
-            },
-            "MY_VAR": {
-                type: 'var',
-                value: 'my-var-value'
-            },
-            "MY_JSON_VAR": {
-                type: 'var',
-                value: {
-                    typeOfVar: 'JSON',
-                    hello: 'world'
-                }
-            },
-            "MY_WORKER": {
-                type: 'service',
-                service: {
-                    name: 'hello-worker'
-                }
-            },
-            "MY_DO": {
-                type: 'durable-object',
-                service: {
-                    name: 'do_worker',
-                },
-                className: 'DurableObjectClass'
-            },
-            "MY_D1": {
-                type: 'd1',
-                databaseName: 'MY_D1'
-            },
-            "MY_R2": {
-                type: 'r2',
-                bucketName: 'MY_R2'
-            }
-        }
-	});
+	setupDevBindings();
 }
