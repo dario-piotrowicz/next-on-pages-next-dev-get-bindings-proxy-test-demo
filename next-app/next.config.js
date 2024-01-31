e0b51b0f81610f875bb075d0db0ff6a4949e9a84
Dario Piotrowicz <dario@cloudflare.com>
2024-01-31 11:36:42 +0000

    use latest prerelease and new setupDevPlatform utility

--- next-app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-if (process.env.NODE_ENV === 'development') {
-	const { setupDevBindings } = require('@cloudflare/next-on-pages/next-dev');
-	setupDevBindings();
-}
---
+++ next-app/next.config.mjs
@@ -0,0 +1,10 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+if (process.env.NODE_ENV === 'development') {
+	setupDevPlatform();
+}
